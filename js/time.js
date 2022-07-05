ab92065afd70f57561a2cc7eaf7b212a0055420e
Chaoci <charlieischao@gmail.com>
2022-07-05 22:49:28 +0800

    7/5

---
+++ js/time.js
@@ -0,0 +1,31 @@
+// ending date and current date
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+
+const newYears= '29 OCT 2022';
+
+function countdown(){
+    const newYearsDate = new Date(newYears); //時間先設定好預計要的日期，並注意格式
+    const currentDate = new Date();//目前的時間
+
+    const totalseconds = (newYearsDate - currentDate) /1000;
+    const days = Math.floor(totalseconds /86400 );
+	const hours = Math.floor(totalseconds /3600) % 24;
+    const mins = Math.floor(totalseconds/60) %60;
+    const seconds = Math.floor(totalseconds)%60;
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+}
+countdown();
+
+
+setInterval(countdown,1000); //
+
+
